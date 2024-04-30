8c8ce976d54fed25e272ea143a4754c645f7aa04
m.baysarov <m.baysarov@bi.zone>
2024-04-30 19:08:06 +0300

    add contenthash into bundle file name, setup html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,27 @@
 const path = require('path');
 const CopyPlugin = require('copy-webpack-plugin');
+const HtmlPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/main.js',
   output: {
-    filename: "build.js",
+    filename: "build.[contenthash].js",
     path: path.resolve(__dirname, 'build'),
     clean: true,
   },
   devtool: 'source-map',
   plugins: [
     new CopyPlugin({
-      patterns: [{from: 'public'}]
-    })
+      patterns: [{
+        from: 'public',
+        globOptions: {
+          ignore: ['**/index.html']
+        }
+      }],
+    }),
+    new HtmlPlugin({
+      template: "public/index.html"
+    }),
   ],
   module: {
     rules: [
